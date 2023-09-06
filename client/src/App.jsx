960b092c5e192260a76fc247861c9a76c2dd3e40
Toluwanee <toluwalope9@gmail.com>
2023-09-06 16:17:43 +0100

    not working yet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,46 @@
-export default function App() {
-  return (
+import { useEffect, useState } from "react"
 
-    <div>
+ function App() {
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    async function fetchTodos() {
+      try {
+        // Make a fetch GET request to your API endpoint
+        const response = await fetch('http://localhost:3000/api/todos');
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        // Handle the data by updating the state
+        setTodos(data);
+        console.log(todos)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchTodos();
+  }, []); 
+
+
+  return (
+  <div>
       <h1 className="text-green-500 font-semibold text-2x1 mt-4 mb-3"> To-do List</h1>
       <input type="text"/> <button type="button">Delete</button>
       <ul>
-        <li><input type="checkbox"/> Foodie <button type="button">Delete</button></li>
+        {todos.map((todo)=>{
+        <li key={todo.id}>{todo}<input type="checkbox"/> {todo.task} <button type="button">Delete</button></li>
+
+        })}
         <li><input type="checkbox"/> Foodie</li>
         <li><input type="checkbox"/> Foodie</li>
       </ul>
     </div>
   )
 }
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
 import express from "express";
 import sql from "./db.js";
+import cors from "cors"
 
  const app = express();
+
+ const data = [
+   {"id": "1",
+   "task": "Take a bath",
+   "is_completed":true
+   },
+   {"id": "2",
+   "task": "Eat",
+   "is_completed":false
+   },
+   {"id": "3",
+   "task": "Eat some more ",
+   "is_completed":false
+   },
+   {"id": "4",
+   "task": "Rest",
+   "is_completed":true
+   },
+   ]
 
  app.get("/", function (req, res){
     res.send("Hello")
  })
+
+ app.get("/app/todo", function (req, res){
+   res.json(data)
+})
 
  app.listen(5000, () => {
     console.log("server is running on port 5173")
