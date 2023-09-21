f3e1f63e7be9ab6e923b422dc0dd650c9de0aa16
Toluwanee <toluwalope9@gmail.com>
2023-09-21 20:38:53 +0100

    put done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,61 +1,59 @@
-import { useEffect, useRef, useState } from "react";
-import './App.css'
-
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  //const taskRef = useRef();
-
+  const taskRef = useRef();
 
   const [todos, setTodos] = useState([]);
   const [taskInputValue, setTaskInputValue] = useState("");
-  //first use effect
-  
-  // useEffect(() => {
-  //     const response = fetch("http://localhost:3000/api/todos").then()
-  //   },[]);
+
 
   useEffect(() => {
     async function fetchTodos() {
       try {
         // Make a fetch GET request to your API endpoint
         const response = await fetch("http://localhost:3000/api/todos");
-        
 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
 
         const data = await response.json();
+        // Handle the data by updating the state
         setTodos(data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     }
-    fetchTodos();
-  }, []);   
 
+    fetchTodos();
+  }, []);
+
+  
   async function handleAddTodoSubmit(event) {
     event.preventDefault();
-    try{
-        const response = await fetch("http://localhost:3000/api/todos2", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            task: taskInputValue,
-            is_completed: false
-          }),
-        });
-        const data = await response.json()
+    try {
+      const response = await fetch("http://localhost:3000/api/todos2", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          task: taskInputValue,
+          is_completed: false,
+        }),
+      });
 
-      // Clear the input field after adding the task
-      setTaskInputValue("");
-      // setTodos((oldTodos) => ([...oldTodos, ...data]))
-      setTodos((todos) => todos.concat(data));
-       } catch (error) {
-        console.error('Error submitting data:', error);
-    }
+      // get response in json
+      // eslint-disable-next-line no-unused-vars
+      const data = await response.json();
+       // Clear the input field after adding the task
+       setTaskInputValue("");
+       // setTodos((oldTodos) => ([...oldTodos, ...data]))
+       setTodos((todos) => todos.concat(data));
+
+      // setting data
+    } catch (error) {}
   }
 
   async function deleteItem(id) {
@@ -74,68 +72,128 @@ function App() {
       console.error("Error deleting todo:", error);
     }
   }
-  
+  const [editTaskId, setEditTaskId] = useState(null);
+  const [editedTaskText, setEditedTaskText] = useState("");
 
-  return (    
+  function startEdit(id, taskText) {
+    setEditTaskId(id);
+    setEditedTaskText(taskText);
+  }
+  
+  function cancelEdit() {
+    setEditTaskId(null);
+    setEditedTaskText("");
+  }
+  async function saveEdit(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/api/todos2/${id}`, {
+        method: "PUT", // Use PUT method to update the task
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          task: editedTaskText,
+          is_completed: false, // You can set this based on your logic
+        }),
+      });
+  
+      if (response.ok) {
+        // Update the task in the local state (todos)
+        setTodos((prevTodos) =>
+          prevTodos.map((todo) =>
+            todo.id === id ? { ...todo, task: editedTaskText } : todo
+          )
+        );
+  
+        // Reset edit state
+        cancelEdit();
+      } else {
+        console.error("Failed to update todo");
+      }
+    } catch (error) {
+      console.error("Error updating todo:", error);
+    }
+  }
+  return (
     <div className="max-w-xl mx-auto p-4">
-  <h1 className="text-green-500 font-semibold text-4xl mt-4 mb-6">To-do List</h1>
-  <form onSubmit={handleAddTodoSubmit} className="mb-4">
-    <div className="flex">
-      <input
-        className="border p-2 flex-1 rounded-l"
-        type="text"
-        name="task"
-        value={taskInputValue}
-        onChange={(event) => {
-          setTaskInputValue(event.target.value);
-        }}
-        placeholder="Add a task..."
-      />
-      <button
-        type="submit"
-        className="bg-green-500 text-white px-4 py-2 rounded-r hover:bg-green-600"
-      >
-        Add
-      </button>
-    </div>
-  </form>
-  <ul className="list-disc p-4">
+    <h1 className="text-green-500 font-semibold text-4xl mt-4 mb-6">To-do List</h1>
+    <form onSubmit={handleAddTodoSubmit} className="mb-4">
+      <div className="flex">
+        <input
+          className="border p-2 flex-1 rounded-l"
+          type="text"
+          name="task"
+          value={taskInputValue}
+          onChange={(event) => {
+            setTaskInputValue(event.target.value);
+          }}
+          placeholder="Add a task..."
+        />
+        <button
+          type="submit"
+          className="bg-green-500 text-white px-4 py-2 rounded-r hover:bg-green-600"
+        >
+          Add
+        </button>
+      </div>
+    </form>
+    <ul className="list-disc pl-4">
     {todos.map((todo, i) => (
       <li key={i} className="flex items-center mb-2">
         <input
           type="checkbox"
           className="mr-2 form-checkbox h-5 w-5 text-green-500"
         />
-        <span className="flex-1">{todo.task}</span>
-        <button
-          type="button"
-          onClick={() => deleteItem(todo.id)}
-          className="text-red-600 hover:text-red-800"
-        >
-          Delete
-        </button>
+        {editTaskId === todo.id ? (
+          <>
+            <input
+              type="text"
+              className="flex-1 border rounded p-1"
+              value={editedTaskText}
+              onChange={(event) => setEditedTaskText(event.target.value)}
+            />
+            <button
+              type="button"
+              onClick={() => saveEdit(todo.id)}
+              className="text-blue-500 hover:text-blue-700 pl-2"
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              onClick={cancelEdit}
+              className="text-red-500 hover:text-red-700 pl-2"
+            >
+              Cancel
+            </button>
+          </>
+        ) : (
+          <>
+            <span className="flex-1">{todo.task}</span>
+            <button
+              type="button"
+              onClick={() => startEdit(todo.id, todo.task)}
+              className="text-green-500 hover:text-green-700 pr-5"
+            >
+              Edit
+            </button>
+            <button
+              type="button"
+              onClick={() => deleteItem(todo.id)}
+              className="text-red-500 hover:text-red-700"
+            >
+              Delete
+            </button>
+          </>
+        )}
       </li>
     ))}
   </ul>
-</div>
+  </div>
 
-    // <div>
-    // <h1 className="text-green-500 font-semibold text-2xl mt-4 mb-3">To-do List</h1>  
-    //   <form onSubmit={handleAddTodoSubmit}>
-    //     <input className="border" type="text" name="task" value={taskInputValue}
-    //     onChange={(event) =>
-    //     {setTaskInputValue(event.target.value)}
-    //       }/>        
-    //       {/* <input type="text" ref={taskRef} name="task" /> */}
-    //     <button type="submit">Add</button>
-    //   </form>      
-    //   <ul>      
-    //     {todos.map((todo, i)=>(
-    //       <li key={i} ><input type="checkbox"/> {todo.task} <button type="button" onClick={() => deleteItem(todo.id)}>Delete</button> </li>
-    //     )) }
-    //   </ul>
+
     // </div>
-  )
+  );
 }
 
-export default App
+export default App;
